f5c1139f26f71a6b6dec3408b70de2a6786699ce
unknown <sylvasam24@gmail.com>
2022-01-03 11:45:24 -0800

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h2>We Begin</h2>
-    </div>
+      <Navbar />
+     </div>
   );
 }
 
